657a7945e432fb9810e5f4e3738f059cd2271292
lisawebcoder <lisa@thefriendsnetwork.ca>
2022-12-18 21:19:20 -0500

    dec18th2022 simply modified to add examUser for my login and register added folder

--- connection.php
+++ connection.php
@@ -1,6 +1,8 @@
 <?php
 $db_host="localhost"; //localhost server 
-$db_user="root";	//database username
+#$db_user="root";	//database username
+//dec18th2022--add examUser
+$db_user="examUser";	//database username
 $db_password="hello";	//database password   
 $db_name="php_pdo_login_db";	//database name
 
